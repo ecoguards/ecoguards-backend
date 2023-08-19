d78112634393fb12e32e822a83c1b3f82c313091
joy <joyagbo49@gmail.com>
2023-08-19 10:53:04 +0100

    Updated model

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const  mongoose  = require("mongoose");
+const {isEmail} = require("validator");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true,'please enter your fullname'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate:[isEmail, 'please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'please enter a password'],
+    unique: true,
+    minlength: [8,'minimum password length is 8 character']
+    
+  },
+  countryOfResidence: {
+    type: String,
+    required: [true, 'please enter your country of residence'],
+  },
+  
+},
+{
+ timestamps:true  
+}
+);
+const User = mongoose.model('User',userSchema);
+module.exports = {User};
