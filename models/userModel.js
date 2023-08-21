7a3dfcba288b9f708709dc01f20233b41c04d9e5
joy <joyagbo49@gmail.com>
2023-08-21 12:05:33 +0100

    Updated userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,35 +1,42 @@
-const  mongoose  = require("mongoose");
-const {isEmail} = require("validator");
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true,'please enter your fullname'],
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, 'please enter your email'],
-    unique: true,
-    lowercase: true,
-    validate:[isEmail, 'please enter a valid email']
-  },
-  password: {
-    type: String,
-    required: [true, 'please enter a password'],
-    unique: true,
-    minlength: [8,'minimum password length is 8 character']
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "please enter your fullname"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "please enter your email"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "please enter a password"],
+      unique: true,
+      minlength: [8, "minimum password length is 8 character"],
+    },
+    countryOfResidence: {
+      type: String,
+      required: [true, "please enter your country of residence"],
+    },
+
     
   },
-  countryOfResidence: {
-    type: String,
-    required: [true, 'please enter your country of residence'],
-  },
   
-},
-{
- timestamps:true  
-}
+  { 
+    verifed: Boolean 
+
+  },
+
+  {
+    timestamps: true,
+  }
 );
-const User = mongoose.model('User',userSchema);
-module.exports = {User};
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
